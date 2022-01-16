ae8f759a04be180a091f3edaed6bd8ab1fdf1ef9
ANT <68531196+ant-7802@users.noreply.github.com>
2022-01-16 04:29:46 -0500

    Update BaseplateDuper.lua

--- BaseplateDuper.lua
+++ BaseplateDuper.lua
@@ -5,7 +5,8 @@
 
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
-local TextButton = Instance.new("TextButton")
+local SwordDuper = Instance.new("TextButton")
+local BoomboxDuper = Instance.new("TextButton")
 
 --Properties:
 
@@ -18,45 +19,59 @@ Frame.BackgroundColor3 = Color3.fromRGB(93, 93, 93)
 Frame.BorderColor3 = Color3.fromRGB(27, 42, 53)
 Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(0.0916271731, 0, 0.525761068, 0)
-Frame.Size = UDim2.new(0, 186, 0, 71)
+Frame.Size = UDim2.new(0, 186, 0, 152)
 
-TextButton.Parent = Frame
-TextButton.BackgroundColor3 = Color3.fromRGB(79, 79, 79)
-TextButton.BorderSizePixel = 0
-TextButton.Position = UDim2.new(0, 0, 0.243752837, 0)
-TextButton.Size = UDim2.new(0, 186, 0, 53)
-TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "Baseplate Duper - Off"
-TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextButton.TextScaled = true
-TextButton.TextSize = 14.000
-TextButton.TextWrapped = true
+SwordDuper.Name = "Sword Duper"
+SwordDuper.Parent = Frame
+SwordDuper.BackgroundColor3 = Color3.fromRGB(79, 79, 79)
+SwordDuper.BorderSizePixel = 0
+SwordDuper.Position = UDim2.new(0, 0, 0.131910741, 0)
+SwordDuper.Size = UDim2.new(0, 186, 0, 53)
+SwordDuper.Font = Enum.Font.SourceSans
+SwordDuper.Text = "Sword Duper - Off"
+SwordDuper.TextColor3 = Color3.fromRGB(0, 0, 0)
+SwordDuper.TextScaled = true
+SwordDuper.TextSize = 14.000
+SwordDuper.TextWrapped = true
+
+BoomboxDuper.Name = "Boombox Duper"
+BoomboxDuper.Parent = Frame
+BoomboxDuper.BackgroundColor3 = Color3.fromRGB(79, 79, 79)
+BoomboxDuper.BorderSizePixel = 0
+BoomboxDuper.Position = UDim2.new(0, 0, 0.56612128, 0)
+BoomboxDuper.Size = UDim2.new(0, 186, 0, 53)
+BoomboxDuper.Font = Enum.Font.SourceSans
+BoomboxDuper.Text = "Boombox Duper - Off"
+BoomboxDuper.TextColor3 = Color3.fromRGB(0, 0, 0)
+BoomboxDuper.TextScaled = true
+BoomboxDuper.TextSize = 14.000
+BoomboxDuper.TextWrapped = true
 
 -- Scripts:
 
-local function ZTIJDYV_fake_script() -- TextButton.LocalScript 
-	local script = Instance.new('LocalScript', TextButton)
+local function KFRUJ_fake_script() -- SwordDuper.LocalScript 
+	local script = Instance.new('LocalScript', SwordDuper)
 
 	script.Parent.MouseButton1Click:Connect(function()
-		if script.Parent.Text == "Baseplate Duper - Off" then
-			script.Parent.Text = "Baseplate Duper - On" else
-			script.Parent.Text = "Baseplate Duper - Off"
+		if script.Parent.Text == "Sword Duper - Off" then
+			script.Parent.Text = "Sword Duper - On" else
+			script.Parent.Text = "Sword Duper - Off"
 		end
 	end)
 	
 end
-coroutine.wrap(ZTIJDYV_fake_script)()
-local function VGHLJO_fake_script() -- TextButton.LocalScript 
-	local script = Instance.new('LocalScript', TextButton)
+coroutine.wrap(KFRUJ_fake_script)()
+local function FEQUU_fake_script() -- SwordDuper.LocalScript 
+	local script = Instance.new('LocalScript', SwordDuper)
 
 	while wait(0.01) do
-		if script.Parent.Text == "Baseplate Duper - On" then
+		if script.Parent.Text == "Sword Duper - On" then
 	
 	function sussy()
 				game.Players:Chat("-re")
-				wait(0.3)
-				game.Players.LocalPlayer.Backpack.BoomBox.Parent = game.Players.LocalPlayer.Character
-				game.Players.LocalPlayer.Character.BoomBox.Parent = workspace
+				wait(0.15)
+				game.Players.LocalPlayer.Backpack.Foil.Parent = game.Players.LocalPlayer.Character
+				game.Players.LocalPlayer.Character.Foil.Parent = workspace
 			end
 			pcall(sussy)
 	
@@ -67,8 +82,8 @@ local function VGHLJO_fake_script() -- TextButton.LocalScript
 	
 	
 end
-coroutine.wrap(VGHLJO_fake_script)()
-local function AVBG_fake_script() -- Frame.Dragify 
+coroutine.wrap(FEQUU_fake_script)()
+local function YSSQCU_fake_script() -- Frame.Dragify 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UIS = game:GetService("UserInputService")
@@ -109,4 +124,38 @@ local function AVBG_fake_script() -- Frame.Dragify
 	
 	dragify(script.Parent)
 end
-coroutine.wrap(AVBG_fake_script)()
+coroutine.wrap(YSSQCU_fake_script)()
+local function TDZVI_fake_script() -- BoomboxDuper.LocalScript 
+	local script = Instance.new('LocalScript', BoomboxDuper)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		if script.Parent.Text == "Boombox Duper - Off" then
+			script.Parent.Text = "Boombox Duper - On" else
+			script.Parent.Text = "Boombox Duper - Off"
+		end
+	end)
+	
+end
+coroutine.wrap(TDZVI_fake_script)()
+local function MCRFWR_fake_script() -- BoomboxDuper.LocalScript 
+	local script = Instance.new('LocalScript', BoomboxDuper)
+
+	while wait(0.01) do
+		if script.Parent.Text == "Boombox Duper - On" then
+	
+	function sussy()
+				game.Players:Chat("-re")
+				wait(0.15)
+				game.Players.LocalPlayer.Backpack.BoomBox.Parent = game.Players.LocalPlayer.Character
+				game.Players.LocalPlayer.Character.BoomBox.Parent = workspace
+			end
+			pcall(sussy)
+	
+		else
+	
+		end
+		end
+	
+	
+end
+coroutine.wrap(MCRFWR_fake_script)()
